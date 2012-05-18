09e43d512dd0859f5517aa6a1e7f8134b7eff840
Mikko Suonio <mikko.suonio@nokia.com>
2012-05-18 10:02:17 +0300

    Add kata

--- topics/about_dates_kata.js
+++ topics/about_dates_kata.js
@@ -1,19 +1,34 @@
 
 module("About dates kata (topics/about_dates_kata.js)");
 
-test("equality", function() {
-});
-
 test("current time", function() {
-    console.log('Current date and time is ' + new Date());
+    console.log('Current date and time is ' + __);
 });
 
-// create date as local time specifying year, month, day, hours, minutes, seconds, ms
-// create date as UTC time specifying year, month, day, hours, minutes, seconds, ms
+test("create date using UTC", function() {
+    var date = __;
+    // year 2012, month 5, day 17, hours 8, minutes 54, seconds 40, milliseconds 123
+    var sameDateInMseconds = 1339923280123;
+    equals(date.valueOf(), sameDateInMseconds, '');
+});
+
+test("create date using local time", function() {
+    var date = __;
+    var sameDateUsingUTC = Date.UTC(2012, 5, 17, 8, 54, 40, 123);
+    equals(date.valueOf(), sameDateUsingUTC, '');
+});
+
+test("create date using millisecond format", function() {
+    var date = __;
+    var sameDateInMseconds = 1339923280123;
+    equals(date.valueOf(), sameDateInMseconds, '');
+});
+
 // create date from millisecond format
 // parse a date time string: ISO format
 // parse a date time string: javascript string format
 // parse a date time string: javascript UTC string format
+// create date from string
 // convert to string using the local time zone
 // convert to string using UTC
 // convert to ISO standard format string
