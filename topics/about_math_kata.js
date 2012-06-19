fc14275acf5c75b27c22d6cc4d35d3af4aff2b61
Mikko Suonio <mikko.suonio@nokia.com>
2012-06-19 15:05:32 +0300

    Add kata

--- topics/about_math_kata.js
+++ topics/about_math_kata.js
@@ -6,4 +6,14 @@ test("random number", function() {
     ok(0 <= x && x < 1, 'a pseudorandom number between 0 and 1'); 
 });
 
+test("round to the nearest integer", function() {
+    function round(x) {
+        return Math.round(x);
+    };
+    equals(round(1.0), 1, 'no need to round');
+    equals(round(1.1), 1, 'round down');
+    equals(round(1.8), 2, 'round up');
+    equals(round(1.5), 2, 'also round up');
+    equals(round(-1.5), -1, 'also round up');
+});
 
