b128b3dedc584a0ad297f26eeac64df5bd540a5c
Mikko Suonio <mikko.suonio@nokia.com>
2012-08-24 10:20:42 +0300

    Add kata

--- topics/about_control_structures_kata.js
+++ topics/about_control_structures_kata.js
@@ -1,6 +1,39 @@
 
 module("About control structures kata (topics/about_control_structures_kata.js)");
 
+test("if", function() {
+    var convert = function(num) {
+        var result;
+        // __
+        return result;
+    };
+    strictEqual(convert(1), 'red', 'can it convert the number?');
+    strictEqual(convert(2), undefined, 'can it convert the number?');
+    strictEqual(convert(3), undefined, 'can it convert the number?');
+});
+
+test("if ... else", function() {
+    var convert = function(num) {
+        var result;
+        // __
+        return result;
+    };
+    strictEqual(convert(1), 'red', 'can it convert the number?');
+    strictEqual(convert(2), 'other', 'can it convert the number?');
+    strictEqual(convert(3), 'other', 'can it convert the number?');
+});
+
+test("if ... else if", function() {
+    var convert = function(num) {
+        var result;
+        // __
+        return result;
+    };
+    strictEqual(convert(1), 'red', 'can it convert the number?');
+    strictEqual(convert(2), 'green', 'can it convert the number?');
+    strictEqual(convert(3), 'other', 'can it convert the number?');
+});
+
 test("switch", function() {
     var convert = function(num) {
         var result;
