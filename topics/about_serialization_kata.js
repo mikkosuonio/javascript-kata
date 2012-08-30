038e12c5600f48b12cf6472a62727512893e7850
Mikko Suonio <mikko.suonio@gmail.com>
2012-08-30 11:19:51 +0300

    Improve kata

--- topics/about_serialization_kata.js
+++ topics/about_serialization_kata.js
@@ -4,20 +4,20 @@ module("Serialization kata (topics/about_serialization_kata.js)");
 test("serialize an object", function() {
     var person = {name: "Mikko", age: 38};
     var serialized = __;
-    equals(serialized, '{"name":"Mikko","age":38}', "how to serialize an object?");
+    strictEqual(serialized, '{"name":"Mikko","age":38}', "how to serialize an object?");
 });
 
 test("deserialize an object", function() {
     var serialized = '{"name":"Mikko"}';
     var person = __;
-    equals(person.name, "Mikko", "is the name restored?");
+    strictEqual(person.name, "Mikko", "is the name restored?");
 });
 
 test("serialize primitive types", function() {
-    equals(__, '"Mikko"', "how to serialize a string?");
-    equals(__, '12.1', "how to serialize a number?");
-    equals(__, 'true', "how to serialize a boolean?");
-    equals(__, 'null', "how to serialize null?");
+    strictEqual(__, '"Mikko"', "how to serialize a string?");
+    strictEqual(__, '12.1', "how to serialize a number?");
+    strictEqual(__, 'true', "how to serialize a boolean?");
+    strictEqual(__, 'null', "how to serialize null?");
 });
 
 test("deserialize primitive types", function() {
@@ -30,7 +30,7 @@ test("deserialize primitive types", function() {
 test("serialize an array", function() {
     var array = [1, 2];
     var serialized = __;
-    equals(serialized, '[1,2]', "how to serialize an array?");
+    strictEqual(serialized, '[1,2]', "how to serialize an array?");
 });
 
 test("deserialize an array", function() {
@@ -42,7 +42,7 @@ test("deserialize an array", function() {
 test("serialize an object with whitespace for readability", function() {
     var person = {name: "Mikko", age: 38};
     var serialized = __;
-    equals(serialized,
+    strictEqual(serialized,
         '{\n' +
         '  "name": "Mikko",\n' +
         '  "age": 38\n' +
